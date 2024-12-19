feddb44eba6c61208fb987bec76bf50ac816a0fb
Damian Pokorski <damianpokorski@hotmail.com>
2024-12-19 23:03:41 +0000

    Minor refactors

--- source/solvers/202411.ts
+++ source/solvers/202411.ts
@@ -1,30 +1,31 @@
-import { Memoize } from 'typescript-memoize';
 import '../utils';
+import { memoize } from '../utils';
 import { initialize } from '../utils/registry';
 
-class Calc {
-  @Memoize((stone: number, blinks: number) => `${stone};${blinks}`)
-  public static getNumberOfStones(stone: number, blinks: number): number {
+const getNumberOfStones = memoize(
+  (tuple: [number, number]): number => {
+    const [stone, blinks] = tuple;
     // No more blinks - we got 1 stone!
     if (blinks == 0) {
       return 1;
     }
     // 0 -> 1
     if (stone == 0) {
-      return Calc.getNumberOfStones(1, blinks - 1);
+      return getNumberOfStones([1, blinks - 1]);
     }
     // Stones with 2 numbers of digits -> Split into two
     const string = stone.toString();
     if (string.length % 2 == 0) {
       const left = parseInt(string.slice(0, string.length / 2));
       const right = parseInt(string.slice(string.length / 2));
-      return Calc.getNumberOfStones(left, blinks - 1) + Calc.getNumberOfStones(right, blinks - 1);
+      return getNumberOfStones([left, blinks - 1]) + getNumberOfStones([right, blinks - 1]);
     }
 
     // * 2024 anything else
-    return Calc.getNumberOfStones(stone * 2024, blinks - 1);
-  }
-}
+    return getNumberOfStones([stone * 2024, blinks - 1]);
+  },
+  (tuple: [number, number]) => `${tuple[0]};${tuple[1]}`
+);
 
 initialize(__filename, async (part, input) => {
   const stones = input
@@ -34,7 +35,7 @@ initialize(__filename, async (part, input) => {
 
   let numberOfStones = 0;
   for (const stone of stones) {
-    numberOfStones += Calc.getNumberOfStones(stone, part == 1 ? 25 : 75);
+    numberOfStones += getNumberOfStones([stone, part == 1 ? 25 : 75]);
   }
 
   return numberOfStones;
--- source/utils/memoize.utils.ts
+++ source/utils/memoize.utils.ts
@@ -1,13 +1,14 @@
 export const memoize = <T, U>(fn: (arg: T) => U, hashMethod?: (arg: T) => string): ((arg: T) => U) => {
-  const cache = {} as Record<string, U>;
+  const cache = new Map<string, U>();
   if (hashMethod == undefined) {
     hashMethod = (arg: T) => `${arg}`;
   }
   return (arg) => {
     const hash = hashMethod(arg);
-    if (cache[hash] == undefined) {
-      cache[hash] = fn(arg);
+
+    if (cache.has(hash) == false) {
+      cache.set(hash, fn(arg));
     }
-    return cache[hash];
+    return cache.get(hash)!;
   };
 };
