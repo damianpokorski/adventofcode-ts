8a5a137ece57e9165e8a2a22d160de5286ff569c
Damian Pokorski <damianpokorski@hotmail.com>
2025-02-02 10:42:19 +0000

    Day 4 2022 - Camp cleanup

---
+++ source/solvers/202204.ts
@@ -0,0 +1,33 @@
+import '../utils';
+import { initialize } from '../utils/registry';
+
+initialize(__filename, async (part, input) => {
+  return input
+    .map(
+      (line) =>
+        line
+          .split(',')
+          .map((elf) => elf.split('-').fromStringToNumberArray())
+          .flat() as [number, number, number, number]
+    )
+    .filter(([a, b, c, d]) => {
+      // Part 1: Check if any shifts are entirely overlapped by one another
+      if ((a >= c && b <= d) || (c >= a && d <= b)) {
+        return true;
+      }
+      // Part 2: Check if any overlap at all
+      if (part == 2) {
+        const [minB, maxC] = [Math.min(b, d), Math.max(a, c)];
+        return minB >= maxC;
+      }
+      return false;
+    }).length;
+}).tests(
+  `2-4,6-8
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`.split('\n'),
+  2,
+  4
+);
--- source/solvers/index.ts
+++ source/solvers/index.ts
@@ -28,6 +28,7 @@ import './202103';
 import './202104';
 import './202202';
 import './202203';
+import './202204';
 import './202301';
 import './202302';
 import './202303';
