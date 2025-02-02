dfa80ef7bab4b300554c9a677408d2b02bd70434
Damian Pokorski <damianpokorski@hotmail.com>
2025-02-02 11:09:33 +0000

    Minor optimsation of one of the slower days

--- source/solvers/201904.ts
+++ source/solvers/201904.ts
@@ -5,6 +5,16 @@ initialize(__filename, async (part, input) => {
   const [min, max] = input.join('').split('-').fromStringToNumberArray();
   let valid = 0;
   for (let i = min; i <= max; i++) {
+    // Minor perf boost, skips 100000 - 111111, 200000 - 222222 etc.
+    // if (i % 100000 == 0) {
+    //   i = parseInt(i.toString().split('').shift()!.repeat(6));
+    // }
+    // if (i % 100000 !== 0 && i % 10000 == 0) {
+    //   console.log(i);
+    //   i = parseInt(i.toString().split('').shift()!.repeat(5));
+    //   console.log({ after: i });
+    // }
+
     const str = i.toString().split('').fromStringToNumberArray();
     // Group by digits
     const groups = str.groupByToEntries((digit) => digit);
@@ -13,6 +23,9 @@ initialize(__filename, async (part, input) => {
     for (let j = 1; j < str.length; j++) {
       if (str[j - 1] > str[j]) {
         consecutive = false;
+        // Here we detected the non consqutive value, we can bump it to next correct value i.e. 10000 to 11111, ~1000ms difference
+        i =
+          parseInt(str.map((digit, index) => (index < j ? digit : Math.max(digit, str[j - 1]))).join('')) - 1;
         break;
       }
     }
